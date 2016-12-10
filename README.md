a0dfdd86f21e10ce7b785b4513bf5ab1dcfbcd77
Vulliard Loan <lv3895@neuf.fr>
2016-12-10 15:03:11 +0900

    Added a classification version of the algorithm

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # KNN
-Simple KNN implementation.
+Simple KNN implementation for both classification and regression.
 Example: glass dataset (http://archive.ics.uci.edu/ml/datasets/Glass_Identification)
--- knn.py
+++ knn.py
@@ -3,10 +3,10 @@
 ##################################### Help ####################################
 """Simple KNN implementation
 Usage:
-  knn.py
-  knn.py -n
+  knn.py [-cn]
   knn.py --help
 Options:
+  -c                          Classification mode
   -n                          Normalize each column
   -h --help                   Show this screen.
 """
@@ -24,14 +24,25 @@ import matplotlib.pyplot as plt
 class KNN:
 	"""A K-nearest neighbors regression"""
 	
-	def __init__(self, pts):
+	def __init__(self, pts, c):
 		self.points = pts
+		self.classif = c
+		if c:
+			self.maxlabel = int(max(data[self.points,10]))
 
 	def predict(self, pt):
 		dist = np.array([np.linalg.norm(data[refPoint,1:10] - data[pt,1:10]) for refPoint in self.points])
 		dist_sort = dist.argsort()
 		nearest_points = [self.points[i] for i in dist_sort[:K]]
-		return sum(data[nearest_points,10])/K
+		# Classification
+		if self.classif:
+			labCount = np.empty(self.maxlabel+1)
+			for i in xrange(self.maxlabel+1):
+				labCount[i] = list(data[nearest_points,10]).count(i)
+			return (-labCount).argsort()[0]
+		# Regression
+		else:
+			return sum(data[nearest_points,10])/K
 		
 
 
@@ -44,6 +55,7 @@ arguments = docopt(__doc__)
 DATA_FILE_NAME = "/home/koala/Documents/Scripts/KNN/KNN/glass.data"
 NB_CROSSVAL = 10
 NORM = arguments["-n"]
+CLASSIF = arguments["-c"]
 
 # Load data
 data_file = open(DATA_FILE_NAME)
@@ -70,10 +82,11 @@ for K in xrange(1,26):
 	KNNs = []
 	SSE = []
 	accuracy = []
+	
 	for i in xrange(NB_CROSSVAL):
 		test_indices = range(training_length*i, training_length*(i+1))
 		training_indices = [k for k in range(len(data)) if k not in test_indices]
-		KNNs.append(KNN(training_indices))
+		KNNs.append(KNN(training_indices, CLASSIF))
 		predictions = np.array([KNNs[i].predict(j) for j in test_indices])
 		expected = np.array([data[j,10] for j in test_indices])
 		SSE.append(sum((predictions - expected)**2))
@@ -81,11 +94,11 @@ for K in xrange(1,26):
 	meanSSE.append(sum(SSE)/NB_CROSSVAL)
 	meanAccuracy.append(sum(accuracy)/NB_CROSSVAL)
 print meanSSE
-print meanAccuracy
 plt.plot(range(1,26), meanSSE)
 plt.xlabel('K')
 plt.ylabel('Mean SSE')
 plt.show()
+print meanAccuracy
 plt.plot(range(1,26), meanAccuracy)
 plt.xlabel('K')
 plt.ylabel('Mean perfect accuracy')
